98e502ec4a2a6c5ef68f7cbacf2d994c38cff76a
hamza aboshhiwa <42789638+Hamza-Shewa@users.noreply.github.com>
2022-02-22 01:25:19 +0200

    small updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,11 @@
 import 'dart:io';
-import 'dart:math';
 import 'dart:ui';
 
-import 'package:demo/admin_landing.dart';
-import 'package:demo/landing.dart';
 import 'package:demo/res_table/cell_button.dart';
 import 'package:demo/res_table/expanded_cell.dart';
 import 'package:demo/res_table/expanded_cell_child.dart';
 import 'package:demo/res_table/res_table.dart';
-import 'package:demo/sign-in-up.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 import 'res_table/cell.dart';
 import 'res_table/nav_buttons.dart';
@@ -47,7 +42,7 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
+class _MyHomePageState extends State<MyHomePage> {
   String text = 'signOut';
 
   update() {
@@ -56,164 +51,137 @@ class _MyHomePageState extends State<MyHomePage> with WidgetsBindingObserver {
     });
   }
 
-  SingletonFlutterWindow? size;
-
-  @override
-  void initState() {
-    size = WidgetsBinding.instance!.window;
-    WidgetsBinding.instance!.addObserver(this);
-    print(Platform.operatingSystemVersion);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    WidgetsBinding.instance!.removeObserver(this);
-    super.dispose();
-  }
-
-  @override
-  void didChangeMetrics() {
-    setState(() {
-      size = WidgetsBinding.instance!.window;
-    });
-  }
-
-
   @override
   Widget build(BuildContext context) {
-    print(size!.viewConfiguration.geometry);
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
         elevation: 0,
       ),
-      body:Flex(
-              direction: Axis.horizontal,
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(vertical: 8),
-                    color: Colors.blue,
-                    child: Column(
-                      children: [
-                        Flexible(
-                          child: ListTile(
-                            title: CircleAvatar(
-                              child: Text(
-                                'A',
-                                style: Theme.of(context).textTheme.bodyText2,
-                              ),
-                              radius: 30,
-                            ),
-                            subtitle: RichText(
-                              text: TextSpan(
-                                children: [
-                                  TextSpan(
-                                    text: 'Test\n',
-                                    style:
-                                        Theme.of(context).textTheme.headline6,
-                                  ),
-                                  TextSpan(
-                                    text: 'Manager',
-                                    style:
-                                        Theme.of(context).textTheme.bodyText2,
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ),
+      body: Flex(
+        direction: Axis.horizontal,
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 8),
+              color: Colors.blue,
+              child: Column(
+                children: [
+                  Flexible(
+                    child: ListTile(
+                      title: CircleAvatar(
+                        child: Text(
+                          'A',
+                          style: Theme.of(context).textTheme.bodyText2,
                         ),
-                        const NavButtons('Home'),
-                        const NavButtons('Employee Management'),
-                        const NavButtons('Management page 3'),
-                        const NavButtons('Management page 4'),
-                        const NavButtons('Settings'),
-                        const Spacer(),
-                        NavButtons(text),
-                      ],
+                        radius: 30,
+                      ),
+                      subtitle: RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: 'Test\n',
+                              style: Theme.of(context).textTheme.headline6,
+                            ),
+                            TextSpan(
+                              text: 'Manager',
+                              style: Theme.of(context).textTheme.bodyText2,
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  flex: 8,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          alignment: Alignment.topRight,
-                          child: const NavButtons('add new employee'),
-                        ),
-                        ResTable(
-                          header: const [
-                            Cell(text: 'name'),
-                            Cell(text: 'age'),
-                            Cell(text: 'address'),
-                            Cell(text: 'nationality'),
-                            Cell(text: 'Edit'),
-                            Cell(text: 'Delete'),
+                  const NavButtons('Home'),
+                  const NavButtons('Employee Management'),
+                  const NavButtons('Management page 3'),
+                  const NavButtons('Management page 4'),
+                  const NavButtons('Settings'),
+                  const Spacer(),
+                  NavButtons(text),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 8,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    alignment: Alignment.topRight,
+                    child: const NavButtons('add new employee'),
+                  ),
+                  ResTable(
+                    header: const [
+                      Cell(text: 'name'),
+                      Cell(text: 'age'),
+                      Cell(text: 'address'),
+                      Cell(text: 'nationality'),
+                      Cell(text: 'Edit'),
+                      Cell(text: 'Delete'),
+                    ],
+                    children: [
+                      for (int i = 0; i < 25; i++)
+                        ExpandedCell(
+                          title: [
+                            Cell(text: 'name $i'),
+                            Cell(text: 'age $i'),
+                            Cell(text: 'city $i'),
+                            Cell(text: 'national $i'),
+                            CellButton(
+                              child: const Icon(
+                                Icons.edit,
+                                color: Colors.green,
+                              ),
+                              onPressed: update,
+                            ),
+                            CellButton(
+                              child: const Icon(
+                                Icons.delete,
+                                color: Colors.red,
+                              ),
+                              onPressed: () {
+                                setState(
+                                  () {
+                                    text = 'deleted';
+                                  },
+                                );
+                              },
+                            ),
                           ],
                           children: [
-                            for (int i = 0; i < 25; i++)
-                              ExpandedCell(
-                                title: [
-                                  Cell(text: 'name $i'),
-                                  Cell(text: 'age $i'),
-                                  Cell(text: 'city $i'),
-                                  Cell(text: 'national $i'),
-                                  CellButton(
-                                    child: const Icon(
-                                      Icons.edit,
-                                      color: Colors.green,
-                                    ),
-                                    onPressed: update,
-                                  ),
-                                  CellButton(
-                                    child: const Icon(
-                                      Icons.delete,
-                                      color: Colors.red,
-                                    ),
-                                    onPressed: () {
-                                      setState(
-                                        () {
-                                          text = 'deleted';
-                                        },
-                                      );
-                                    },
-                                  ),
-                                ],
-                                children: [
-                                  ExpandedCellChild(
-                                    child: ListTile(
-                                      title: const Text('license number\n'),
-                                      isThreeLine: true,
-                                      subtitle: Text('license $i'),
-                                    ),
-                                  ),
-                                  ExpandedCellChild(
-                                      child: ListTile(
-                                    title: const Text('phone number\n'),
-                                    subtitle: Text('00218$i'),
-                                  )),
-                                  ExpandedCellChild(
-                                    child: ListTile(
-                                      title: const Text('place of birth\n'),
-                                      subtitle: Text('place of birth $i'),
-                                    ),
-                                  ),
-                                ],
-                              )
+                            ExpandedCellChild(
+                              child: ListTile(
+                                title: const Text('license number\n'),
+                                isThreeLine: true,
+                                subtitle: Text('license $i'),
+                              ),
+                            ),
+                            ExpandedCellChild(
+                                child: ListTile(
+                              title: const Text('phone number\n'),
+                              subtitle: Text('00218$i'),
+                            )),
+                            ExpandedCellChild(
+                              child: ListTile(
+                                title: const Text('place of birth\n'),
+                                subtitle: Text('place of birth $i'),
+                              ),
+                            ),
                           ],
-                        ),
-                      ],
-                    ),
+                        )
+                    ],
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
+          ),
+        ],
+      ),
     );
   }
 }
